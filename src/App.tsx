7ec8abc3bf0c477a691dc7c32e6cb72fea0dfcfb
Valentin BEAUGET <valentin.beauget@gmail.com>
2024-08-06 17:44:28 +0200

    Started building arbo

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@
 function App() {
   return (
     <>
-      <h1>Bienvenue sur BledMarket</h1>
+      <header>
+        <nav></nav>
+      </header>
+      <main>
+        <h1>Bienvenue sur BledMarket</h1>
+      </main>
+      <footer>c - BledMarket 2024</footer>
     </>
   );
 }
